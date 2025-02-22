9bd0e7396ce45368fd7f08e050c248e4743f5d0f
portal-ui <109450499+portal-ui@users.noreply.github.com>
2025-02-22 21:37:12 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const elements = document.querySelectorAll(".hidden");
+
+elements.forEach((element, index) => {
+  setTimeout(() => {
+    element.classList.remove("hidden");
+    element.classList.add("animate__animated", "animate__fadeInLeft");
+  }, index * 300);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".card");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            entry.target.classList.add("animate__fadeInUp");
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0)";
+          }, index * 200);
+        }
+      });
+    },
+    { threshold: 0.3 }
+  );
+
+  cards.forEach((card) => {
+    observer.observe(card);
+  });
+});
