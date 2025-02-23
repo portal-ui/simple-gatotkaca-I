c4e355d6900e91aa1aeef6b460346a44896661ff
portal-ui <109450499+portal-ui@users.noreply.github.com>
2025-02-24 01:17:51 +0700

    update pages, dan penambahan konten

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,14 @@ document.addEventListener("DOMContentLoaded", function () {
     observer.observe(card);
   });
 });
+
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
